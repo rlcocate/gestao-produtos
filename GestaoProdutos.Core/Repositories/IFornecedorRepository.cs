b8b4777c573433e64bb6b3b31d99250571416936
Rodrigo Luiz Cocate <rodrigo.cocate@hotmail.com>
2024-01-15 23:26:36 -0300

    Limpeza de código (Fornecedor)

--- GestaoProdutos.Core/Repositories/IFornecedorRepository.cs
+++ GestaoProdutos.Core/Repositories/IFornecedorRepository.cs
@@ -1,13 +1,9 @@
 ﻿using GestaoProdutos.Core.Entities;
 using GestaoProdutos.Core.Repositories.Base;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 
 namespace GestaoProdutos.Core.Repositories
 {
     public interface IFornecedorRepository : IRepositoryBaseAsync<Fornecedor>
     {
-        Task<IReadOnlyList<Fornecedor>> GetAll();
-        Task<Fornecedor> GetById(int id);
     }
 }
--- GestaoProdutos.Infra/Repositories/FornecedorRepository.cs
+++ GestaoProdutos.Infra/Repositories/FornecedorRepository.cs
@@ -2,9 +2,6 @@
 using GestaoProdutos.Core.Repositories;
 using GestaoProdutos.Infra.DB.Persistence;
 using GestaoProdutos.Infra.Repositories.Base;
-using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 
 namespace GestaoProdutos.Infra.Repositories
 {
@@ -12,16 +9,6 @@ namespace GestaoProdutos.Infra.Repositories
     {
         public FornecedorRepository(GestaoProdutosContext context) : base(context)
         {
-        }
-
-        public Task<IReadOnlyList<Fornecedor>> GetAll()
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<Fornecedor> GetById(int id)
-        {
-            throw new NotImplementedException();
         }
     }
 }
