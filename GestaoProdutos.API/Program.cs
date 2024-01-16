22d3d81ab1e7d50b698e6190928296ebdf2fbb68
Rodrigo Luiz Cocate <rodrigo.cocate@hotmail.com>
2024-01-16 13:55:00 -0300

    Limpeza de código

--- GestaoProdutos.API/Program.cs
+++ GestaoProdutos.API/Program.cs
@@ -8,16 +8,9 @@ namespace GestaoProdutos.API
     {
         public static void Main(string[] args)
         {
-            //CreateHostBuilder(args).Build().Run();
             CreateWebHostBuilder(args).Build().Run();
         }
-
-        //public static IHostBuilder CreateHostBuilder(string[] args) =>
-        //    Host.CreateDefaultBuilder(args)
-        //        .ConfigureWebHostDefaults(webBuilder =>
-        //        {
-        //            webBuilder.UseStartup<Startup>();
-        //        });
+        
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args).UseStartup<Startup>();
     }
